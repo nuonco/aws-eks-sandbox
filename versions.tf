1871d936265fbed2ac229ae864a8ef3eb4de8e1a
Jordan Acosta <jordan@nuon.co>
2025-08-14 18:27:08 -0700

    chore: upgrade aws provider version to meet constraints (#43)

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "= 5.94.1"
+      version = "= 6.8.0"
     }
     helm = {
       source  = "hashicorp/helm"
